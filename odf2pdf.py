454e6b45fca4c2852c6360f7f316edd00ee3f49b
J. Eduardo <j.eduardo@gmail.com>
2016-03-18 22:36:46 +0100

    Changed response headers to specify that responses must not be cached.

--- odf2pdf.py
+++ odf2pdf.py
@@ -10,6 +10,8 @@ from flask import Flask, jsonify, make_response, request, send_file
 
 from libreoffice import LibreOffice
 
+from nocache import nocache
+
 VALID_MIME_TYPES = {
     'application/vnd.oasis.opendocument.text': 'odt',
     'application/vnd.openxmlformats-officedocument.wordprocessingml.document': 'docx',
@@ -47,8 +49,8 @@ logger.debug("Debug mode is enabled")
 backend = LibreOffice()
 logger.info("odf2pdf converter ready to process requests")
 
-
 @app.route('/api/v1/pdf', methods = ['POST'])
+@nocache
 def convert_pdf():
     content_type = request.headers.get('Content-type')
     if content_type not in VALID_MIME_TYPES:
